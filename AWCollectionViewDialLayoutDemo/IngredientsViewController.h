de934b0974caee4d38792ba2e4d481dbb8e60c00
Kevin Byrne <kevin@byrne.io>
2014-04-12 04:40:54 -0700

    Added functionality for ingredients page text fields.

--- AWCollectionViewDialLayoutDemo/IngredientsViewController.h
+++ AWCollectionViewDialLayoutDemo/IngredientsViewController.h
@@ -14,5 +14,14 @@
 - (IBAction)menuPressed:(id)sender;
 @property (weak, nonatomic) IBOutlet UIButton *cornerButton;
 @property (strong, nonatomic) ALRadialMenu *cornerMenu;
+- (IBAction)addIngred:(id)sender;
+- (IBAction)removeIngred:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *removeButton;
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *firstIngred;
+@property (weak, nonatomic) IBOutlet UITextField *secondIngred;
+@property (weak, nonatomic) IBOutlet UITextField *thirdIngred;
+@property (weak, nonatomic) IBOutlet UITextField *fourthIngred;
 
 @end
